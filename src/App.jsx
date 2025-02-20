4330e025af1a099d2564ce5f0635f9caf7daf128
Tamim Ahmed <tamim20072@gmail.com>
2025-02-20 07:11:27 +0600

    install and setup shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
       <h1 className="text-3xl">Vite + React</h1>
+      <Button variant="outline" >Click Me</Button>
     </>
   )
 }
